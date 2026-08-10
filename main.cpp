d08d574d14c93aa88a36cdbb9fbbc167318cdab3
tamzishe <tamzsiwira@gmail.com>
2026-08-09 22:13:21 -0600

    qt project creation

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "apos.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "APOS1_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    APOS w;
+    w.show();
+    return QApplication::exec();
+}
